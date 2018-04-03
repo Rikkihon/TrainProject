a584cd4400be32cba2222453bc8cdf59c308a5bd
Rikki Honnold <rikki@rikkis-mbp.lan>
2018-04-03 14:40:35 -0500

    Adding more features

--- New Train Time.js
+++ New Train Time.js
@@ -8,11 +8,33 @@ var config = {
   storageBucket: "myfirstproject-68441.appspot.com",
   messagingSenderId: "247363885714"
 };
+
 firebase.initializeApp(config);
 var database = firebase.database();
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+
+});
+ 
 
 // Grabs user input
-var trainName = $("#employee-name-input").val().trim();
+var trainName = $("#train-name-input").val().trim();
 var destination = $("#role-input").val().trim();
 var frequency = $("#rate-input").val().trim();
 
@@ -22,9 +44,11 @@ moment().fromNow();
 // 2. Button for adding trains
 $("#add-train-btn").on("click", function (event) {
   event.preventDefault();
-  var trainName = $("#employee-name-input").val().trim();
+  var trainName = $("#train-name-input").val().trim();
   var destination = $("#role-input").val().trim();
   var frequency = $("#rate-input").val().trim();
+  //added some data inputs that are a bit sloppy, TBH
+
   if(isNaN(frequency)){
 alert("Not a number in 'Frequency'-field!");
   }
@@ -55,9 +79,6 @@ alert("Not a number in 'Frequency'-field!");
 
   var frequency = $("#rate-input").val().trim();
 
-  // Time is 3:30 AM
-  // var firstTime = "03:30";
-
   console.log(firstTimeConverted);
 
   // Current Time
@@ -81,73 +102,33 @@ alert("Not a number in 'Frequency'-field!");
 
   console.log("Time of next train"+ timeNextTrain);
   // Creates local "temporary" object for holding employee data
+  //had to stringify otherwise the system wouldn't allow the moment.js conversions in the object
   var newTrain = {
     name: trainName,
     destination: destination,
     ft: JSON.stringify(firstTimeConverted),
     frequency: frequency,
-    //convertedDate: convertedDate,
     time: JSON.stringify(currentTime),
     minutesAway: diffTime
   };
 
-  console.log(newTrain);
 
 
   database.ref("/trains").push(newTrain);
   $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
-    frequency + "</td><td>" + timeNextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+    frequency + "</td><td>" + timeNextTrain + "</td><td>" + tMinutesTillTrain + "</td>" + "<td><button>Delete</button></td>" + "<td><button>Edit</button></td></tr>");
 
   // Alert
   alert("Train successfully added");
 
   // Clears all of the text-boxes
-  $("#employee-name-input").val("");
+  $("#train-name-input").val("");
   $("#role-input").val("");
   $("#start-input").val("");
   $("#rate-input").val("");
 
-
-  //database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-  //database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(childSnapshot) {
-  //console.log(childSnapshot.val());
-
+ //then we could write the "Delete" and "Edit" buttons in 
 
 });
 
-  //switch to UNIX times and then calculate 
-  //use moment.js
-
-    // Assume the following situations.
-
-    // (TEST 1)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 3 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:18 -- 2 minutes away
-
-    // (TEST 2)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 7 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:21 -- 5 minutes away
-
-
-    // ==========================================================
-
-    // Solved Mathematically
-    // Test case 1:
-    // 16 - 00 = 16
-    // 16 % 3 = 1 (Modulus is the remainder)
-    // 3 - 1 = 2 minutes away
-    // 2 + 3:16 = 3:18
-
-    // Solved Mathematically
-    // Test case 2:
-    // 16 - 00 = 16
-    // 16 % 7 = 2 (Modulus is the remainder)
-    // 7 - 2 = 5 minutes away
-    // 5 + 3:16 = 3:21
+  
